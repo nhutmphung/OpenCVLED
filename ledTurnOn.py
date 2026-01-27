ce57b0d7de86c5d3ec12bb8fd48fb919f6c5f360
Nhut Phung <nhutmphugn@gmail.com>
2026-01-27 06:36:43 -0600

    added text and flipped img

--- ledTurnOn.py
+++ ledTurnOn.py
@@ -5,13 +5,30 @@ cap = cv.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()         #reads camera/webcam
+
+    img = cv.flip(frame, 1)
     width = int(cap.get(3))         #gets width for webcam
     height = int(cap.get(4))        #gets height for webcam
 
-    img = cv.rectangle(frame, (100, 100), (200, 200), (0, 0, 255), -1)    
-    img = cv.rectangle(frame, (400, 100), (500, 200), (0, 0, 0), -1)    
+    #red square in top left corner
+    cv.rectangle(img, (0, 0), (100, 100), (0, 0, 255), -1)  
+    cv.putText(img, "RED", (0, 50), cv.FONT_HERSHEY_COMPLEX_SMALL, 2, (0,0,0), 2, cv.LINE_AA)
+    
+    #green square in top right corner
+    cv.rectangle(img, (width-100, 0), (width, 100), (0, 255, 0), -1)
+    cv.putText(img, "GREEN", (width-100, 50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (0,0,0), 2, cv.LINE_AA)    
 
-    cv.imshow('frame', img)
+    #blue square in bottom left corner
+    cv.rectangle(img, (0, height-100), (100, height), (255, 0, 0), -1)  
+    cv.putText(img, "BLUE", (0, height-50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (0,0,0), 2, cv.LINE_AA) 
+
+    #white square in bottom right corner
+    cv.rectangle(img, (width-100, height-100), (width, height), (255, 255, 255), -1) 
+    cv.putText(img, "WHITE", (width-100, height-50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (0,0,0), 2, cv.LINE_AA)
+
+        
+
+    cv.imshow('Output', img)
 
 
     if cv.waitKey(1) == ord('q'):
