50a1617334863603ee6ab4e37a4217782a6c22f6
Nhut Phung <nhutmphugn@gmail.com>
2026-02-02 20:25:04 -0600

    made functions out of old spahghet code

--- ledTurnOn.py
+++ ledTurnOn.py
@@ -17,6 +17,66 @@ time.sleep(2)   # wait for serial to initialize')
 latest_result = None
 lastPrintTime = 0
 
+def colored_squares():
+        #red square in top left corner
+    cv.rectangle(frame, (0, 0), (rectangle_width,rectangle_height), (0, 0, 255), -1)  
+    
+    #green square in top right corner
+    cv.rectangle(frame, (width-rectangle_width, 0), (width, rectangle_height), (0, 255, 0), -1)
+
+    #blue square in bottom left corner
+    cv.rectangle(frame, (0, height-rectangle_height), (rectangle_width, height), (255, 0, 0), -1)  
+
+    #white square in bottom right corner
+    cv.rectangle(frame, (width-rectangle_width, height-rectangle_height), (width, height), (255, 255, 255), -1) 
+
+def text_on_squares():
+        #text in red square in top left corner
+    cv.putText(frame, "RED", (0, 50), cv.FONT_HERSHEY_COMPLEX_SMALL, 2, (0,0,0), 2, cv.LINE_AA)
+    
+    #text in green square in top right corner
+    cv.putText(frame, "GREEN", (width-rectangle_width, 50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (0,0,0), 2, cv.LINE_AA)    
+
+    #text in blue square in bottom left corner
+    cv.putText(frame, "BLUE", (0, height-50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (0,0,0), 2, cv.LINE_AA) 
+
+    #text in white square in bottom right corner
+    cv.putText(frame, "WHITE", (width-rectangle_width, height-50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (0,0,0), 2, cv.LINE_AA)
+
+
+def zone_detection(): 
+            # Convert normalized coordinates to pixel coordinates
+    x = int((1.0 - landmark.x)* frame.shape[1])
+    y = int(landmark.y * frame.shape[0])
+    # points = cv.circle(frame, (x, y), 5, (0, 255, 0), -1)
+
+    red, green, blue = 0, 0, 0 
+
+    if 0 <= x <= rectangle_width and 0 <= y <= rectangle_height: 
+        red = 255
+        rgb_message = f"{red},{green},{blue}"
+        # ser.write(rgb_message.encode())
+        print("RED Zone Detected")
+
+    elif (width - rectangle_width) <= x <= width and 0 <= y <= rectangle_height:
+        green = 255
+        rgb_message = f"{red},{green},{blue}"
+        # ser.write(rgb_message.encode())
+        print("GREEN Zone Detected")
+
+    elif 0 <= x <= rectangle_width and (height - rectangle_height) <= y <= height:
+        blue = 255
+        rgb_message = f"{red},{green},{blue}"
+        # ser.write(rgb_message.encode())
+        print("BLUE Zone Detected")
+
+    elif (width - rectangle_width) <= x <= width and (height - rectangle_height) <= y <= height:
+        white = 255
+        rgb_message = f"{red},{green},{blue}"
+        # ser.write(rgb_message.encode())
+        print("WHITE Zone Detected")
+     
+
 # Callback function for live stream mode
 def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
     global latest_result, lastPrintTime
@@ -38,6 +98,7 @@ options = HandLandmarkerOptions(
     min_hand_presence_confidence=0.5,
     min_tracking_confidence=0.5,
     result_callback=print_result)
+
 
 
 
@@ -70,21 +131,8 @@ with HandLandmarker.create_from_options(options) as landmarker:
         rectangle_width = 100
         rectangle_height = 100 
 
-        #red square in top left corner
-        cv.rectangle(frame, (0, 0), (rectangle_width,rectangle_height), (0, 0, 255), -1)  
-        cv.putText(frame, "RED", (0, 50), cv.FONT_HERSHEY_COMPLEX_SMALL, 2, (0,0,0), 2, cv.LINE_AA)
-        
-        #green square in top right corner
-        cv.rectangle(frame, (width-rectangle_width, 0), (width, rectangle_height), (0, 255, 0), -1)
-        cv.putText(frame, "GREEN", (width-rectangle_width, 50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (0,0,0), 2, cv.LINE_AA)    
-
-        #blue square in bottom left corner
-        cv.rectangle(frame, (0, height-rectangle_height), (rectangle_width, height), (255, 0, 0), -1)  
-        cv.putText(frame, "BLUE", (0, height-50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (0,0,0), 2, cv.LINE_AA) 
-
-        #white square in bottom right corner
-        cv.rectangle(frame, (width-rectangle_width, height-rectangle_height), (width, height), (255, 255, 255), -1) 
-        cv.putText(frame, "WHITE", (width-rectangle_width, height-50), cv.FONT_HERSHEY_COMPLEX_SMALL, 1.25, (0,0,0), 2, cv.LINE_AA)
+        colored_squares()
+        text_on_squares()
         
         # Draw landmarks if available
         if latest_result and latest_result.hand_landmarks:
@@ -92,35 +140,7 @@ with HandLandmarker.create_from_options(options) as landmarker:
                 # Draw each landmark
                 for landmark in hand_landmarks:
                     # Convert normalized coordinates to pixel coordinates
-                    x = int((1.0 - landmark.x)* frame.shape[1])
-                    y = int(landmark.y * frame.shape[0])
-                    points = cv.circle(frame, (x, y), 5, (0, 255, 0), -1)
-
-                    red, green, blue = 0, 0, 0 
-
-                    if 0 <= x <= rectangle_width and 0 <= y <= rectangle_height: 
-                        red = 255
-                        rgb_message = f"{red},{green},{blue}"
-                        # ser.write(rgb_message.encode())
-                        print("RED Zone Detected")
-
-                    elif (width - rectangle_width) <= x <= width and 0 <= y <= rectangle_height:
-                        green = 255
-                        rgb_message = f"{red},{green},{blue}"
-                        # ser.write(rgb_message.encode())
-                        print("GREEN Zone Detected")
-
-                    elif 0 <= x <= rectangle_width and (height - rectangle_height) <= y <= height:
-                        blue = 255
-                        rgb_message = f"{red},{green},{blue}"
-                        # ser.write(rgb_message.encode())
-                        print("BLUE Zone Detected")
-
-                    elif (width - rectangle_width) <= x <= width and (height - rectangle_height) <= y <= height:
-                        white = 255
-                        rgb_message = f"{red},{green},{blue}"
-                        # ser.write(rgb_message.encode())
-                        print("WHITE Zone Detected")
+                   zone_detection()
 
                 
                 
@@ -129,7 +149,7 @@ with HandLandmarker.create_from_options(options) as landmarker:
         
         # Display FPS
         cv.putText(frame, f'Hands detected: {len(latest_result.hand_landmarks) if latest_result and latest_result.hand_landmarks else 0}', 
-                    (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    (150, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         
         # Show the frame
         cv.imshow('Hand Tracking', frame)
